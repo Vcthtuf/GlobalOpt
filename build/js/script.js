95f3092c1fc141a24502e7e975240b6fac85dc58
Vcthtuf <vcthtuf@gmail.com>
2020-11-22 17:22:31 +0500

    Slider ready

--- build/js/script.js
+++ build/js/script.js
@@ -18,31 +18,50 @@ window.addEventListener('DOMContentLoaded', () => {
             menu.classList.toggle('promo___menu_active');
         })
     })
+
+
+    //= hamburger
+
+    // slider 
+
+    let track = document.querySelector('.slider__track');
+    let prev = document.querySelector('.slider__button_prev');
+    let next = document.querySelector('.slider__button_next');
+    let trackShift = 0;
+    let itemWidth = 600;
+
+
+    prev.addEventListener('click', function () {
+        track.style.cssText = 'transform: translateX(' + (trackShift + itemWidth) + 'px);';
+        trackShift += itemWidth;
+
+        checkButton();
+
+    });
+
+    next.addEventListener('click', function () {
+        track.style.cssText = 'transform: translateX(' + (trackShift - itemWidth) + 'px);';
+        trackShift -= itemWidth;
+
+        checkButton();
+
+    });
+
+    function checkButton() {
+        if (trackShift >= 600) {
+            prev.classList.add("button_passive");
+        } else {
+            prev.classList.remove("button_passive");
+        }
+
+        if (trackShift <= -600) {
+            next.classList.add("button_passive");
+        } else {
+            next.classList.remove("button_passive");
+        }
+
+    }
+
+    //= slider
+
 })
-
-//= hamburger
-
-// slider 
-
-let track = document.querySelector('.slider__track');
-let prev = document.querySelector('.slider__button_prev');
-let next = document.querySelector('.slider__button_next');
-let trackShift = 0;
-let itemWidth = 600;
-
-
-prev.addEventListener('click', function () {
-    track.style.cssText = 'transform: translateX(' + (trackShift + itemWidth) + 'px);';
-    trackShift += itemWidth;
-    console.log(trackShift + itemWidth);
-
-});
-
-next.addEventListener('click', function () {
-    track.style.cssText = 'transform: translateX(' + (trackShift - itemWidth) + 'px);';
-    trackShift -= itemWidth;
-    console.log(trackShift - itemWidth);
-
-});
-
-//=
